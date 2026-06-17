3f244b54735f09773ce96bd599b91b7b61d8f857
Divyansh Vashishtth <divyanshvashishtth123@gmail.com>
2026-06-17 16:16:08 +0530

    add String.js file contain string code

---
+++ javaScript-main/basics/String.js
@@ -0,0 +1,27 @@
+const name ="Divyansh Vashishtth";
+const repoCount = 3;
+
+// String interpolation-
+console.log(`My name is ${name} and my repository count is ${repoCount}`);
+
+
+const gameName = new String('Phantom007');
+console.log(gameName);
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+
+console.log(gameName.indexOf('P'));
+
+
+
+
+
+
+
